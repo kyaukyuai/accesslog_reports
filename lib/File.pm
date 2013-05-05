e44c19fcdb108ddd930e83910798fcd85dccf007
Yuya Kakui <mail@kakuiyuya.com>
2013-05-05 09:40:08 +0900

    nabuchi report

---
+++ lib/File.pm
@@ -0,0 +1,21 @@
+package File;
+
+use warnings;
+use strict;
+use Logger;
+
+sub new {
+  my $class = shift;
+  my $self = {};
+  return bless $self, $class;
+}
+
+sub read {
+  my ($self, $file) = @_;
+  open (FILE, $file) or die Logger->error("$!");
+  while (<FILE>) {
+  }
+  close (FILE);
+}
+
+1;
---
+++ reportAccesslog.pl
@@ -0,0 +1,44 @@
+#!/home/kakuiyuya01/perl5/perlbrew/perls/perl-5.16.3/bin/perl
+use warnings;
+use strict;
+use lib "/home/kakuiyuya01/work/reports/lib";
+use Date;
+use Mail;
+use Logger;
+
+# crete new logger
+my $logger = new Logger();
+
+# get report date
+my ($YYYY, $MM, $DD) = Date->get_yesterday();
+
+# set mail config
+my $accesslog = "/var/log/httpd/accesslog.$YYYY$MM$DD";
+my $sendmail  = "";
+my $from      = "y.kakui\@gmail.com";
+my $to        = "y.kakui\@gmail.com";
+my $subject   = "[Report] accesslog.$YYYY$MM$DD";
+
+# report accesslog
+$logger->info("Start Calculating $accesslog");
+my $accessCount = 0;
+if (-e $accesslog) {
+  open (LOG, $accesslog) or die $logger->error("$!");
+  while(<LOG>) {
+    $accessCount ++;
+  }
+  close (LOG);
+}
+$logger->info("Finished Calculating $accesslog");
+
+my $msg =
+<< "_TEXT_";
+accessCount = $accessCount
+_TEXT_
+
+# send mail
+$logger->info("Start send mail");
+my $mail = new Mail($to, $from, $subject);
+$logger->info("Finished send mail");
+
+1;
